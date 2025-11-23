533fce4e3d7926c02c0748fd7dad07d77cf2b363
cmh1008 <cmh1008@alu.ubu.es>
2025-11-23 13:23:38 +0100

    combio y actualizacion del codigo respecto a los datos ya que no existe tidyjson

--- proceso.R
+++ proceso.R
@@ -9,7 +9,6 @@ path_depresion_pais       <- "INPUT/Data/TODO/Dep1.1.json"
 path_depresion_educacion  <- "INPUT/Data/TODO/Dep2.1.json"
 path_egresados            <- "INPUT/Data/TODO/Egre1.1.json"
 path_matriculados         <- "INPUT/Data/TODO/MatriRama.json"
-path_sonido               <- "INPUT/Data/TODO/sonido.json"
 path_alcohol              <- "INPUT/Data/TODO/alcohol.json"
 
 # --- Cargar JSON originales ---
@@ -17,7 +16,6 @@ json_data1 <- fromJSON(file = path_depresion_pais)
 json_data2 <- fromJSON(file = path_depresion_educacion)
 json_data3 <- fromJSON(file = path_egresados)
 json_data4 <- fromJSON(file = path_matriculados)
-json_data5 <- fromJSON(file = path_sonido)
 json_data6 <- fromJSON(file = path_alcohol)
 
 # --- Función para aplanar coord + point ---
@@ -33,7 +31,6 @@ df_depresion_pais_limpia      <- aplanar_coord_point(json_data1)
 df_depresion_educacion_limpia <- aplanar_coord_point(json_data2)
 df_egresados_limpia           <- aplanar_coord_point(json_data3)
 df_matriculados_limpia        <- aplanar_coord_point(json_data4)
-df_sonidos_limpia             <- aplanar_coord_point(json_data5)
 df_alcohol_limpia             <- aplanar_coord_point(json_data6)
 
 # --- Crear carpeta de salida ---
@@ -56,14 +53,9 @@ write_json(df_matriculados_limpia,
            "OUTPUT/JSON_Planos/Matriculados_flat.json",
            pretty = TRUE, auto_unbox = TRUE)
 
-write_json(df_sonidos_limpia,
-           "OUTPUT/JSON_Planos/Sonidos_flat.json",
-           pretty = TRUE, auto_unbox = TRUE)
-
 write_json(df_alcohol_limpia,
            "OUTPUT/JSON_Planos/Alcohol_flat.json",
            pretty = TRUE, auto_unbox = TRUE)
 
 # --- Verificación ---
-print(names(df_sonidos_limpia))
 print(head(df_depresion_pais_limpia))
